ee6714550f282c19fc81cae41c5c968befad8763
Thuthao <ngothithuthao202128@gmail.com>
2024-06-24 21:59:17 +0700

    room detail view

---
+++ room/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from .models import Room
+
+@login_required
+def rooms(request):
+  rooms = Room.objects.all()
+
+  return render(request, 'room/rooms.html', {'rooms': rooms})
+
+@login_required
+def room(request, slug):
+  rooms = Room.objects.get(slug=slug)
+
+  return render(request, 'room/rooms.html', {'rooms': rooms})
+
+
